d2a3ca1fe12abf956ec3bd5fa7963badc654125e
Gean <gean@test.com>
2022-08-10 15:08:27 -0300

    Pesquisa Veiculo

---
+++ src/main/java/br/com/etechoracio/etecquery/controller/VeiculoController.java
@@ -0,0 +1,56 @@
+package br.com.etechoracio.etecquery.controller;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.etechoracio.etecquery.dao.VeiculoDAO;
+import br.com.etechoracio.etecquery.enums.TipoCambioEnum;
+import br.com.etechoracio.etecquery.model.Veiculo;
+
+@RestController
+@RequestMapping("/veiculos")
+public class VeiculoController {
+	
+	@Autowired
+	private VeiculoDAO dao;
+	
+	@GetMapping("/cambio/{cambio}")
+	public List<Veiculo> buscarPorTipoCambio(@PathVariable TipoCambioEnum cambio) {
+		return dao.findByTipoCambio(cambio) ;
+	}
+	
+	@GetMapping("/fabricante/{fabricante}")
+	public List<Veiculo> buscarPorFabricante(@PathVariable String fabricante) {
+		return dao.findByFabricante(fabricante);
+	}
+	
+	@GetMapping("/cambio/{cambio}/fabricante/{fabricante}")
+	public List<Veiculo> buscarPorTipoCambioEFabricante(@PathVariable TipoCambioEnum cambio,
+														@PathVariable String fabricante) {
+		return dao.findByTipoCambioAndFabricante(cambio, fabricante);
+	}
+	
+	@GetMapping("/ano-fabricacao/{anoFabricacao}")
+	public List<Veiculo> buscarPorAnoFabricacao(@PathVariable Integer anoFabricacao) {
+		return dao.findByAnoFabricacao(anoFabricacao);
+	}
+	
+	@GetMapping("/valor")
+	public List<Veiculo> buscarPorValor(@RequestParam BigDecimal min, 
+										@RequestParam BigDecimal max) {
+		return dao.findByValorBetween(min, max);    
+	}
+	
+	@GetMapping("/telefones")
+	public List<Veiculo> buscarComTelefones() {
+		return dao.findByTelefonesIsNotNull();
+	}
+	
+}
---
+++ src/main/java/br/com/etechoracio/etecquery/dao/VeiculoDAO.java
@@ -0,0 +1,18 @@
+package br.com.etechoracio.etecquery.dao;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.etechoracio.etecquery.enums.TipoCambioEnum;
+import br.com.etechoracio.etecquery.model.Veiculo;
+
+public interface VeiculoDAO extends JpaRepository<Veiculo, Long> {
+	List<Veiculo> findByTipoCambio(TipoCambioEnum tipo);
+	List<Veiculo> findByFabricante(String fabricante);
+	List<Veiculo> findByTipoCambioAndFabricante(TipoCambioEnum tipo, String fabricante);
+	List<Veiculo> findByAnoFabricacao(Integer ano);
+	List<Veiculo> findByValorBetween(BigDecimal min, BigDecimal max);
+	List<Veiculo> findByTelefonesIsNotNull();
+}
